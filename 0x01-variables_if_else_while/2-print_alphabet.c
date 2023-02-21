2616ea8fd69ba16efc19ba85a40bdfb7e1613924
Mkango265 <alinafe@ntsmw.tech>
2023-02-20 16:04:20 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry program - print alphabet in small letters
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n')
+		return (0);
+}
