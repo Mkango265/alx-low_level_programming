6fed4c815bbda518539cad17cc21b78bb40335ac
Mkango265 <alinafe@ntsmw.tech>
2023-02-20 17:05:00 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description - print single digit numbersstarting from 0 to 10
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; 0 <= 10; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
