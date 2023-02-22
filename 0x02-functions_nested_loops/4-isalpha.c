f017638b83238ab9b7e75cd714eb5b83ea795c24
Mkango265 <alinafe@ntsmw.tech>
2023-02-22 06:05:07 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,9 @@
  * @c: The character in ASCII code
  * Return: 1 if c is a letter or 0 otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-	if ((c >= 97 && <= 122) || (c >= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
