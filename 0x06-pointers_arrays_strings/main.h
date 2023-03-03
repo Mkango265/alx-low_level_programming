26e91d5705a02a8a288a477f4c550b234a0ecc2d
Mkango265 <alinafe@ntsmw.tech>
2023-03-03 15:32:44 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* File: main.h
+* Auth: Alinafe Kankhumbwa
+* Desc: Header file containing prototypes of functions
+* Used in the 0x06. C - More pointers, arrays and strings directory.
+*/
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -6,4 +17,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-int _putchar(char c);
+
+#endif
