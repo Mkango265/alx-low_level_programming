26aead26e968b2fc64e042f9c4801d5f5f6d26d5
Mkango265 <alinafe@ntsmw.tech>
2023-03-07 13:32:27 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* File: main.h
+* Auth: Alinafe Kankhumbwa
+* Desc: Header file containing prototypes of functions
+* Used in the 0x07. C - Even more pointers, arrays and strings directory.
+*/
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
+#endif
