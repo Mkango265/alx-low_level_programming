54759bbded277246eea80dc32a610d8134b14fb5
Mkango265 <alinafe@ntsmw.tech>
2023-03-21 19:21:41 -0700

    3-function_like_macro.h update

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,9 @@
 #ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MARCO_H
-
+#define FUNCTION_LIKE_MACRO_H
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
+#define SUM(x, y) ((x) + (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
-#endif
+#endif /* FUNCTION_LIKE_MACRO_H */
+
